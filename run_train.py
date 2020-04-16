0ec6915c95b925ac9fd99afd1e7a1438ecbf7c80
dbaranchuk <dmitrybaranchuk@gmail.com>
2020-04-17 02:39:19 +0300

    add inception loss

--- run_train.py
+++ run_train.py
@@ -98,7 +98,7 @@ def main():
         trainer.start_from_checkpoint(deformator, shift_predictor)
         for target_id in range(trainer.p.max_latent_ind):
             kls[target_id] = trainer.eval(G, deformator, shift_predictor, inception, target_id)
-        np.save("fixed/inspection_dim_kl.npy", kls)
+        np.save(f"fixed/inspection_dim_{args.target_class}_kl.npy", kls)
 
 
 if __name__ == '__main__':
