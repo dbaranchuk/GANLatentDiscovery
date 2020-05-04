8f765c7af6f615b37b5ce31921c8d0a3b29eed82
dbaranchuk <dmitrybaranchuk@gmail.com>
2020-05-04 13:17:29 +0300

    add inception loss

--- trainer.py
+++ trainer.py
@@ -67,7 +67,7 @@ class Trainer(object):
                 orig_imgs = F.interpolate(orig_imgs, size=(299, 299),
                                      mode='bilinear', align_corners=False)
                 feats = inception(orig_imgs)
-                orig_dists[i * batch_size: (i+1) * batch_size] = (target_feats - feats).norm(2, dim=-1).mean(-1)
+                orig_dists[i * batch_size: (i+1) * batch_size] = (target_feats - feats).norm(2, dim=-1)
             print(orig_dists)
             nearest_sample = orig_dists.argmin().item()
 
