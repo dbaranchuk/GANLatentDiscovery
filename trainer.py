1aefc66a5025fbc8acb36bed0a0f3ea4cd55bf46
dbaranchuk <dmitrybaranchuk@gmail.com>
2020-05-13 10:19:12 +0300

    add efros loss

--- trainer.py
+++ trainer.py
@@ -55,32 +55,31 @@ class Trainer(object):
         mean = torch.tensor([0.485, 0.456, 0.406]).cuda()[..., None, None]
         std = torch.tensor([0.229, 0.224, 0.225]).cuda()[..., None, None]
 
-        sample_dir = f'efros_dataset_200/val/{class_idx}'
-        os.makedirs("efros_dataset_200", exist_ok=True)
-        os.makedirs("efros_dataset_200/val", exist_ok=True)
+        sample_dir = f'efros_dataset_gz_200/val/{class_idx}'
+        os.makedirs("efros_dataset_gz_200", exist_ok=True)
+        os.makedirs("efros_dataset_gz_200/val", exist_ok=True)
         os.makedirs(sample_dir, exist_ok=True)
 
-        orig_sample_dir = f'orig_efros_dataset_200/val/{class_idx}'
-        os.makedirs("orig_efros_dataset_200", exist_ok=True)
-        os.makedirs("orig_efros_dataset_200/val", exist_ok=True)
+        orig_sample_dir = f'orig_efros_dataset_gz_200/val/{class_idx}'
+        os.makedirs("orig_efros_dataset_gz_200", exist_ok=True)
+        os.makedirs("orig_efros_dataset_gz_200/val", exist_ok=True)
         os.makedirs(orig_sample_dir, exist_ok=True)
 
         G.target_classes.data = torch.tensor(class_idx).cuda()
 
         for batch_id in range(2):
             print(class_idx, batch_id)
-            z_orig = make_noise(self.p.batch_size, G.dim_z).cuda()
-            z_adv = nn.Parameter(z_orig, requires_grad=True)
-            optimizer = torch.optim.Adam([z_adv], lr=0.003, betas=(0.9, 0.999))
 
+            z_orig = make_noise(self.p.batch_size, G.dim_z).cuda()
             # Original samples
-            orig_samples = G(z_orig)
+            orig_samples = G(z_orig).detach()
+            imgs_efros = orig_samples.clone()
+            optimizer = torch.optim.Adam([imgs_efros], lr=0.003, betas=(0.9, 0.999))
 
             for step in range(0, self.p.n_steps, 1):
                 G.zero_grad()
                 optimizer.zero_grad()
 
-                imgs_efros = G(z_adv)
                 imgs_adv = ((imgs_efros + 1.) / 2.).clamp(0, 1)
                 imgs_adv = F.interpolate(imgs_adv, size=(224, 224),
                                          mode='bilinear', align_corners=False)
@@ -104,7 +103,7 @@ class Trainer(object):
                         orig_img = to_image(orig_samples[i])
                         orig_img.save(os.path.join(orig_sample_dir, f'{batch_id * 25 + i}.png'))
 
-                    with PdfPages(f"efros_samples/{class_idx}_{batch_id}_step{step}.pdf") as pdf:
+                    with PdfPages(f"efros_dataset_gz_200/{class_idx}_{batch_id}_step{step}.pdf") as pdf:
                         fig, axes = plt.subplots(len(imgs_efros), 3, figsize=(20, 200))
                         for i in range(len(imgs_efros)):
                             axes[i][0].imshow(to_image(orig_samples[i]))
