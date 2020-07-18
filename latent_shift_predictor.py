08d8f31ea47921d48de86c0fd17996f3038bbfbc
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2020-07-19 00:05:36 +0300

    face-identity

--- latent_shift_predictor.py
+++ latent_shift_predictor.py
@@ -13,10 +13,10 @@ class ResNetPredictor(nn.Module):
     def __init__(self, dim, downsample=None):
         super(ResNetPredictor, self).__init__()
         self.features_extractor = resnet50(pretrained=False)
-        self.features_extractor.conv1 = nn.Conv2d(
-            6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        nn.init.kaiming_normal_(self.features_extractor.conv1.weight,
-                                mode='fan_out', nonlinearity='relu')
+        # self.features_extractor.conv1 = nn.Conv2d(
+        #     6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        # nn.init.kaiming_normal_(self.features_extractor.conv1.weight,
+        #                         mode='fan_out', nonlinearity='relu')
 
         self.features = self.features_extractor.avgpool
         self.features.register_forward_hook(save_hook)
@@ -30,9 +30,13 @@ class ResNetPredictor(nn.Module):
         batch_size = x1.shape[0]
         if self.downsample is not None:
             x1, x2 = F.interpolate(x1, self.downsample), F.interpolate(x2, self.downsample)
-        self.features_extractor(torch.cat([x1, x2], dim=1))
-        features = self.features.output.view([batch_size, -1])
 
+        self.features_extractor(x1, dim=1)
+        features1 = self.features.output.view([batch_size, -1])
+        self.features_extractor(x2, dim=1)
+        features2 = self.features.output.view([batch_size, -1])
+
+        features = features1 + features2
         logits = self.type_estimator(features)
         shift = self.shift_estimator(features)
 
