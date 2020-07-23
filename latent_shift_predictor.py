fb0cec185bcf59d6c29b46b041400f6849034570
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2020-07-23 20:16:08 +0300

    face-identity

--- latent_shift_predictor.py
+++ latent_shift_predictor.py
@@ -41,7 +41,7 @@ class SiameseResNetPredictor(nn.Module):
 class ResNetPredictor(nn.Module):
     def __init__(self, dim, downsample=None):
         super(ResNetPredictor, self).__init__()
-        self.features_extractor = resnet50(pretrained=False)
+        self.features_extractor = resnet34(pretrained=False)
         self.features_extractor.conv1 = nn.Conv2d(
             6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         nn.init.kaiming_normal_(self.features_extractor.conv1.weight,
